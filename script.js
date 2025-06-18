6536186ee61cd3766a7dc5b1c70b164ad377c7e0
Chase Peterson <cjkpeterson@gmail.com>
2025-06-17 21:44:48 -0700

    Achieve basic etch-a-sketch functionality on 16x16 grid

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const container = document.querySelector(".container");
-const height = 16;
-const width = 16;
+let height = 16;
+let width = 16;
 
-function whenHover() {}
+let isMouseDown = false;
+
+document.addEventListener("mousedown", () =>{
+    isMouseDown = true;
+});
+
+document.addEventListener("mouseup", () =>{
+    isMouseDown = false;
+});
+
+function color(e) {
+    e.target.classList.add("colored");
+}
+
+function colorCheck(e) {
+    isMouseDown && color(e);
+}
 
 for (let i = 0; i < width; i++) {
     let col = document.createElement("div");
@@ -10,7 +26,8 @@ for (let i = 0; i < width; i++) {
     for (let i = 0; i < height; i++) {
         let pixel = document.createElement("div");
         pixel.classList.add("pixel");
-        pixel.addEventListener("hover", whenHover);
+        pixel.addEventListener("mousedown", color);
+        pixel.addEventListener("mouseenter", colorCheck);
         col.appendChild(pixel);
     }
     container.appendChild(col);

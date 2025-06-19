0afd8c48a6ee57de22aa734eff144506b3f379ec
Chase Peterson <cjkpeterson@gmail.com>
2025-06-19 11:48:55 -0700

    Add functional lighten and darken buttons

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ const resizeButton = document.querySelector("#resize");
 const picker = document.querySelector("#colorPicker");
 const randomButton = document.querySelector("#random");
 const eraserButton = document.querySelector("#eraser");
+const lightenButton = document.querySelector("#lightener");
+const darkenButton = document.querySelector("#darkener");
 const togglers = document.querySelector("#togglers");
 
 let pixels;
 let isMouseDown = false;
 let isRandom = false;
-let currentColor = "black";
+let currentColor = "rgb(0, 0, 0)";
 
 function getRandomColor(max=255) {
     let red = Math.random() * max;
@@ -22,8 +24,8 @@ function getRandomColor(max=255) {
 const spelt = ["R", "a", "n", "d", "o", "m", " ", "C", "o", "l", "o", "r", "s"];
 spelt.forEach(char => {
     letter = document.createElement("span");
-    letter.style.color = getRandomColor(175); //Makes sure colors aren't too light
-    letter.style.fontWeight = "bold";
+    letter.style.color = getRandomColor(160); //Makes sure colors aren't too light
+    letter.style.fontWeight = "bold"; //Makes it stick out a little more
     letter.textContent = char;
     randomButton.appendChild(letter);
 })
@@ -37,7 +39,7 @@ function toggleOff() {
 
 function toggleTarget(e) {
     let targ = e.target.id;
-    if (!targ) {
+    if (!targ) { //If the target is blank, it's one of the letters of "Random Colors", so we need to go to it's parent
         targ = e.target.parentElement.id;
     }
     let el = document.getElementById(targ);
@@ -66,11 +68,43 @@ picker.addEventListener("input", () => {
   toggleOff();
 });
 
+function changeRGB(pixel, brightness) {
+    const bgColor = window.getComputedStyle(pixel).backgroundColor;
+    console.log(bgColor);
+    const currRGB = bgColor.match(/\d+/g);
+    console.log(currRGB);
+    return currRGB.map( value => {
+        value =  parseInt(value) + brightness;
+        if (value > 255) {
+            value = 255;
+        }
+        else if (value < 0) {
+            value = 0;
+        }
+        return value;
+    });
+}
 
 function color(e) {
     const pixel = e.target;
     if (eraserButton.classList.contains("active")) {
         pixel.classList.add("blank");
+    }
+    else if (lightenButton.classList.contains("active")) {
+        const newRGB = changeRGB(pixel, 25);
+        pixel.style.backgroundColor = `rgb(${newRGB[0]}, ${newRGB[1]}, ${newRGB[2]})`;
+        // Considering checking whether this causes the pixel to become fully white,
+        // and then adding it to "blank," but for now don't see any real need.
+    }
+    else if (darkenButton.classList.contains("active")) {
+        if (pixel.classList.contains("blank")) { //darken it just a little
+            pixel.classList.remove("blank");
+            pixel.style.backgroundColor = "rgb(230, 230, 230)";
+        }
+        else {
+            const newRGB = changeRGB(pixel, -25);
+            pixel.style.backgroundColor = `rgb(${newRGB[0]}, ${newRGB[1]}, ${newRGB[2]})`;
+        }
     }
     else {
         pixel.classList.remove("blank");
@@ -103,10 +137,14 @@ function makeGrid(size=16) {
     pixels = document.querySelectorAll(".pixel");
 }
 
-function clear(e) {
+function reset() {
     picker.value = "#000000";
-    currentColor = "black";
+    currentColor = "rgb(0, 0, 0)";
     toggleOff(); //Reset everything
+}
+
+function clear(e) {
+    reset();
     pixels.forEach(pix => pix.classList.add("blank"));
 }
 
@@ -118,7 +156,7 @@ function resize(e) {
         newSize = parseInt(prompt("Enter the new number of pixels per side (between 1 and 100)", "16"));
     }
     if (newSize) { //Make sure it's not NULL, which would mean the user hit cancel
-        clear(e);
+        reset();
         container.replaceChildren();
         makeGrid(newSize);
     }
